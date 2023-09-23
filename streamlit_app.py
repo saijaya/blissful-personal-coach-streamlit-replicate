9c3bd1da46963e1ea2fedc9c3a68266cb74d00b7
saijaya <saiprasad1413@gmail.com>
2023-09-22 23:28:51 -0700

    increment 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import replicate
 import os
+import pandas
 
-st.title('blissful 🎈')
+# App title
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot: blissful 🎈")
 
 st.write('Sup Shiv!')
